6f64fff189f788634c9ebe74b4749ac6119965ec
eralpkor <eralp.ny@gmail.com>
2020-06-04 18:58:52 -0400

    Finish expence, ceasar

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,7 +1,6 @@
 # Your code here
 cache = {}
 
-
 def expensive_seq(x, y, z):
     if x <= 0:
         if (x, y, z) in cache:
@@ -12,8 +11,10 @@ def expensive_seq(x, y, z):
     if x >  0: 
         if (x, y, z) in cache:
             return cache[(x, y, z)]
-        cache[(x, y, z)] = expensive_seq(x-1,y+1,z) + expensive_seq(x-2,y+2,z*2) + expensive_seq(x-3,y+3,z*3)
+        cache[(x, y, z)] = expensive_seq(x-1,y+1,z) + expensive_seq(x-2,y+2,z*2)\
+            + expensive_seq(x-3,y+3,z*3)
         return cache[(x, y, z)]
+
 
 
 if __name__ == "__main__":
@@ -22,3 +23,5 @@ if __name__ == "__main__":
         print(f"{i*2} {i*3} {i*4} = {x}")
 
     print(expensive_seq(150, 400, 800))
+
+print(cache[0])
